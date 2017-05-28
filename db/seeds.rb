20afc5f6c13ef72ccd349fe01609b775f22a03ea
Maximilian Richt <maxi@richt.name>
2017-05-28 21:16:22 +0200

    add ministry seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ministries = Ministry.create(
+  [
+    { name: 'Justiz und Verbraucherschutz', short_name:' BMJV' },
+    { name: 'Gesundheit', short_name:' BMG' },
+    { name: 'Auswärtiges Amt', short_name:' AA' },
+    { name: 'Wirtschaft und Energie', short_name:' BMWi' },
+    { name: 'Wirtschaftliche Zusammenarbeit und Entwicklung', short_name:' BMZ' },
+    { name: 'Inneres', short_name:' BMI' },
+    { name: 'Finanzen', short_name:' BMF' },
+    { name: 'Arbeit und Soziales', short_name:' BMAS' },
+    { name: 'Umwelt, Naturschutz, Bau und Reaktorsicherheit', short_name:' BMUB' },
+    { name: 'Bildung und Forschung', short_name:' BMBF' },
+    { name: 'Verkehr und digitale Infrastruktur', short_name:' BMVI' },
+    { name: 'Verteidigung', short_name:' BMVg' },
+    { name: 'Familie, Senioren, Frauen und Jugend', short_name:' BMFSFJ' },
+    { name: 'Ernährung und Landwirtschaft', short_name:' BMEL' },
+    { name: 'Kanzleramt und Medien' }
+  ]
+)
