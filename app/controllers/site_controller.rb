a3b9f8ce0fae48d80bd9f2606144370e99bd34b5
Maximilian Richt <maxi@richt.name>
2017-06-01 09:53:30 +0200

    start: use nbsp, filter by letter

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,10 @@
 class SiteController < ApplicationController
   def index
-    @org_count = 42 # FIXME
+    @org_count = Organization.count
     @law_count = Law.count
     @first_comment = Comment.order(asked_at: :asc).first || OpenStruct.new(asked_at: 5.years.ago)
+
+    @letter = 'A'
+    @orgs = Organization.where("LOWER(name) LIKE ?", @letter.downcase + '%')
   end
 end
