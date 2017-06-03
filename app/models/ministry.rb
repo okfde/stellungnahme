604c5cf09c1403629cc623fa5edadd0f71f3385d
Maximilian Richt <maxi@richt.name>
2017-06-03 16:30:39 +0200

    link ministry full name

--- app/models/ministry.rb
+++ app/models/ministry.rb
@@ -4,4 +4,8 @@ class Ministry < ApplicationRecord
   validates_presence_of :name, :slug
 
   has_and_belongs_to_many :laws
+
+  def full_name
+    name + (short_name.blank? ? '' : " (#{short_name})")
+  end
 end
