762a1f95562e38326d2119e67c3e8615fe7bbae7
Maximilian Richt <maxi@richt.name>
2017-06-05 14:12:48 +0200

    comment: document is optional, display source

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :law
   belongs_to :draft
-  belongs_to :document
+  belongs_to :document, optional: true
 
   has_and_belongs_to_many :organizations
 end
