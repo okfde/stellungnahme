596a03341f17e3977b78d42683e23397d99644d0
Maximilian Richt <maxi@richt.name>
2017-06-12 00:38:02 +0200

    law: prefix slug by id, shorten it

--- app/models/law.rb
+++ app/models/law.rb
@@ -1,10 +1,24 @@
 class Law < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :id_and_title, use: :slugged
   validates_presence_of :title, :slug
   validates :title, uniqueness: true
 
   has_and_belongs_to_many :ministries
   has_many :drafts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def should_generate_new_friendly_id?
+    title_changed? || super
+  end
+
+  def id_and_title
+    [
+      [:id, :title]
+    ]
+  end
+
+  def normalize_friendly_id(value)
+    value.to_s.gsub('&', 'und').parameterize.truncate(120, separator: '-', omission: '')
+  end
 end
