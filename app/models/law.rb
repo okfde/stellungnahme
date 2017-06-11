933d4c9ca32dc99dc3d57048a34c026b2bebf5fe
Maximilian Richt <maxi@richt.name>
2017-06-11 22:14:32 +0200

    law: title is unique

--- app/models/law.rb
+++ app/models/law.rb
@@ -2,6 +2,7 @@ class Law < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates_presence_of :title, :slug
+  validates :title, uniqueness: true
 
   has_and_belongs_to_many :ministries
   has_many :drafts, dependent: :destroy
