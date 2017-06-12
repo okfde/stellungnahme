894ede12d26824029d09d38cf4be9717d1f93c9d
Maximilian Richt <maxi@richt.name>
2017-06-12 23:35:30 +0200

    route: ignore documents, shorten comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   resources :organizations, except: :index, path: 'verbaende'
   get '/verbaende', to: redirect('/'), as: :organizations_index
 
-  resources :documents, path: 'd'
+  # resources :documents, path: 'd'
   resources :ministries, path: 'ministerien'
 
   resources :laws, path: 'gesetze' do
     resources :drafts, except: :index
-    resources :comments, except: :index
+    resources :comments, path: 's', except: :index
   end
 
   get '/:letter', to: 'site#index', constraints: { letter: /[[:alpha:]]/ }
