837f8565d5db7f2e56a533457623aae342cc3e20
Maximilian Richt <maxi@richt.name>
2017-06-13 00:15:15 +0200

    up and down

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,23 +5,23 @@ class SiteController < ApplicationController
     @first_comment = Comment.order(asked_at: :asc).first || OpenStruct.new(asked_at: 5.years.ago)
 
     @letters = {}
-    lttr = (('A'..'Z').to_a + %w(Ä Ü Ö))
+    lttr = (('a'..'z').to_a + %w(ä ü ö))
     ICU::Collation.collate('de', lttr).map { |l| @letters[l] = l.downcase }
-    @letters['Ä'] = 'ae'
-    @letters['Ü'] = 'ue'
-    @letters['Ö'] = 'oe'
+    @letters['ä'] = 'ae'
+    @letters['ü'] = 'ue'
+    @letters['ö'] = 'oe'
 
-    @letter = params[:letter] || 'A'
+    @letter = params[:letter] || 'a'
     raise ArgumentError, 'letter must be a single char' if @letter.size > 2
     raise ArgumentError, 'letter unknown' unless @letters.values.include? @letter
 
     case @letter
     when 'ae'
-        @letter = 'Ä'
+        @letter = 'ä'
     when 'ue'
-        @letter = 'Ü'
+        @letter = 'ü'
     when 'oe'
-        @letter = 'Ö'
+        @letter = 'ö'
     end
 
     @orgs = Organization.where('LOWER(name) LIKE ?', @letter.downcase + '%')
